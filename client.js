9f1725f3773b73289a262137fd21941cc02dc180
David Clince <clinced@tcd.ie>
2016-11-01 20:28:10 +0000

    changed host to my node on nebula, formatted the output slightly

--- client.js
+++ client.js
@@ -1,19 +1,19 @@
 
 var net = require('net');
 
-var HOST = 'localhost';
+var HOST = '10.62.0.46';
 var PORT = 8000;
 
 var client = new net.Socket();
 client.connect(PORT, HOST, function() {
 
-	console.log('Connection Successful \n' + 'Connected to: ' + HOST + ':' + PORT);
+	console.log('Connection Successful \n' + 'Connected to: ' + HOST + ':' + PORT + '\n');
 	client.write('GET /echo.php?message=test HTTP/1.1\r\n\r\n');
 });
 
 client.on('data', function(data) {
 
-	console.log('DATA: %s\n\n', data);
+	console.log('DATA: ' + data + '\n');
 
 });
 
